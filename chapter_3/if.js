151323ad1bf4d27d35566e652e6232cebb70b601
Amanda <amandaluana777@gmail.com>
2020-12-08 08:31:29 -0200

    loops; if and else statements

---
+++ chapter_3/if.js
@@ -0,0 +1,26 @@
+// if statements - não é loop
+
+//const age = 25;
+
+//if(age > 20){
+//    console.log('you are over 20 years old');
+//}
+
+
+//const ninjas = ['Amanda', 'Luzi', 'Lua', 'Sol'];
+
+//if(ninjas.length > 2){
+//    console.log("that's a lot of ninjas");
+//}
+
+// else if atatements
+
+const password = 'p@ss'
+
+if(password.length >=12){
+    console.log('that password is mighty strong, congratulation!');
+} else if(password.length >=8){
+    console.log('that password is long enough!');
+} else {
+    console.log('passowrd is not long enough');
+}
--- chapter_3/loops.js
+++ chapter_3/loops.js
@@ -4,12 +4,28 @@
 //     console.log('in loop:' , i);
 // }
 
-// console.log('loop finished');
+// console.log('loop finished');        
 
-const names = ['shaun', 'mario', 'luigi'];
+// const names = ['shaun', 'mario', 'luigi'];
 
-for(let i = 0; i < names.length; i++){
+// for(let i = 0; i < names.length; i++){
     // console.log(names[i]);
-    let html = `<div>${names}</div>`;
-    console.log(html);
-}
+//    let html = `<div>${names}</div>`;
+//    console.log(html);
+//}
+
+//let i = 0;
+
+//while(i < names.length){
+//    console.log('aluno: ', names[i]);
+//    i++;
+//}
+
+// do while loops 
+
+// let i = 3;
+
+// do{
+//    console.log('val of i is: ', i); // executar antes de saber a condição
+//  i++;
+//} while(i < 5);
