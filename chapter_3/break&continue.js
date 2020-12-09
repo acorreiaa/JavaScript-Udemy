3a9142db03993ca3c638aa95ac01655fff4c4b01
Amanda <amandaluana777@gmail.com>
2020-12-09 08:42:34 -0200

    logical operators and break&continues

---
+++ chapter_3/break&continue.js
@@ -0,0 +1,38 @@
+// break and continue
+
+const scores = [50, 25, 0, 100, 20, 10];
+
+for(let i = 0; i < scores.length; i++){
+    if(scores[i] === 0){
+        continue;
+    }
+
+    console.log('tour score ', scores[i]);
+
+    if(scores[i] === 100){
+        console.log('congrats, you got the top score!');
+        break;
+    }
+} 
+
+const grade = 'P';
+
+switch(grade){
+    case 'A':
+        console.log('you got an A!');
+        break;
+    case 'B' :
+        console.log('you got an B!');
+        break;
+    case 'C':
+        console.log('you got an C!')    
+        break;
+    case 'D':
+        console.log('you got an D!');
+        break;
+    case 'E':
+        console.log('you got an E!');
+        break;  
+    default:
+        console.log('not a valid grade');    
+}
--- chapter_3/if.js
+++ chapter_3/if.js
@@ -15,12 +15,32 @@
 
 // else if atatements
 
-const password = 'p@ss'
+const password = 'p@ss12'
 
-if(password.length >=12){
+/*if(password.length >=12){
     console.log('that password is mighty strong, congratulation!');
 } else if(password.length >=8){
     console.log('that password is long enough!');
 } else {
     console.log('passowrd is not long enough');
+}*/
+
+// logical orperators - OR || and AND &&
+
+if(password.length >=12 && password.includes('@')){
+    console.log('that password is mighty strong, congratulation!');
+} else if(password.length >=8 || password.includes('@') && password.length >= 5){
+    console.log('that password is long enough!');
+} else {
+    console.log('passowrd is not long enough');
 }
+
+// logical NOT (!)
+
+// let user = false;
+if(!user){
+    console.log('you must be llogged in to continue');
+}
+
+console.log(!true);
+console.log(!false);n 
