3c4c7ec2500e4fea75356615b5f9f545d97225af
acorreiaa <amandaluana777@gmail.com>
2021-02-03 06:49:06 -0300

    finished mod./chap.9

---
+++ Chapter_9/chaning-array.js
@@ -0,0 +1,19 @@
+const productsB = [
+    {name: 'gold star', prices: 30},
+    {name: 'green shell', prices: 10},
+    {name: 'red shell', prices: 40},
+    {name: 'banana skin', prices: 5},
+    {name: 'mushroom', prices: 50}
+];
+
+//const filtered = products.filter(product => product.price > 20);
+
+//const promos = filtered.map(product => {
+    //return `the ${product.name} is ${product.price / 2} pounds`;
+//});
+
+const promos = products
+    ''.filter(product => product.price > 20)
+    .map(product => `the ${product.name} is ${product.price / 2} prounds`);
+
+console.log(promos);
