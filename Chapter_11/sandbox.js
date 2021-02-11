ab365dde58c2198ec886e7f19c19e7283ee337fe
acorreiaa <amandaluana777@gmail.com>
2021-02-11 06:53:36 -0300

    finished chap.10(w problem), start chap. 11

---
+++ Chapter_11/sandbox.js
@@ -0,0 +1,14 @@
+//dates and times
+const not = new Date();
+
+console.log(now);
+console.log(typeof now);
+
+//year, month, day, times
+console.log('getFullYear:', now.getFullYear());
+console.log('getMOnth:', now.getMonth());
+console.log('getDate:', now.getDate());
+console.log('getDay:', now.getDay());
+console.log('getHours', now.getHours());
+console.log('getMinutes:', now.getMinutes());
+console.log('getSecond:', now.getSecond());
